1609652738f397330dc9260aac71122332bf66b9
pablopalomino96 <pablo.palomino1@alu.uclm.es>
2019-03-02 12:37:19 +0100

    added flask app to manage the services (first approach)

--- SOURCE/app/api_esios.py
+++ SOURCE/app/api_esios.py
@@ -3,25 +3,19 @@
 
 import time
 import requests
-
-TOKEN = '879c5ab5bc0211a1ba23527736e3402a0e708f0a5e7c370f0274004e676ee5f6'
-BASE_URL = 'https://api.esios.ree.es'
-URL = '{}/indicators/$INDICATOR?start_date=$START_DATE&end_date=$END_DATE'.format(BASE_URL)
-
-INDICATOR_SPOT = '613'  # Precio marginal del mercado intradiario (precio de compra/venta a la red eletrica)
-INDICATOR_PVPC = '1013' # Precio Voluntario para el Pequeño Consumidor (precio de venta al cliente)
+import project_constants
 
 headers = {
           'Accept' : 'application/json; application/vnd.esios-api-v1+json',
           'Content-Type' : 'application/json',
           'Host' : 'api.esios.ree.es',
-          'Authorization' : 'Token token=\"' + TOKEN + '\"'
+          'Authorization' : 'Token token=\"' + project_constants.ESIOS_TOKEN + '\"'
           }
 
 def pvpc_price (start_date=time.strftime("20%y/%m/%d"),end_date=time.strftime("20%y/%m/%d")):
     global headers
 
-    url = URL.replace('$INDICATOR',INDICATOR_PVPC).replace('$START_DATE',start_date).replace('$END_DATE',end_date)
+    url = project_constants.ESIOS_URL.replace('$INDICATOR',project_constants.PVPC).replace('$START_DATE',start_date).replace('$END_DATE',end_date)
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         data = response.json()
@@ -34,7 +28,7 @@ def pvpc_price (start_date=time.strftime("20%y/%m/%d"),end_date=time.strftime("2
 def spot_price (start_date=time.strftime("20%y/%m/%d"),end_date=time.strftime("20%y/%m/%d")):
     global headers
 
-    url = URL.replace('$INDICATOR',INDICATOR_SPOT).replace('$START_DATE',start_date).replace('$END_DATE',end_date)
+    url = project_constants.ESIOS_URL.replace('$INDICATOR',project_constants.SPOT).replace('$START_DATE',start_date).replace('$END_DATE',end_date)
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         data = response.json()
@@ -43,8 +37,7 @@ def spot_price (start_date=time.strftime("20%y/%m/%d"),end_date=time.strftime("2
     else:
         print("An error has occurred")
         return None
-    
-'''
+
+
 if "__NAME__==__MAIN__":
     print ("PVPC: {} €/kwh\nSPOT: {} €/kwh".format(pvpc_price(),spot_price()))
-'''
--- SOURCE/app/server.py
+++ SOURCE/app/server.py
@@ -3,7 +3,7 @@
 
 from flask import Flask, make_response, abort, jsonify, request, redirect, flash, url_for, render_template
 
-from api_aemet import aemet_cr_weather
+from api_aemet import aemet_weather
 from api_esios import spot_price, pvpc_price
 
 app = Flask(__name__)
@@ -16,21 +16,21 @@ def index():
 
 @app.route('/weather')
 def get_weather_buffer():
-    w_buffer = aemet_cr_weather()
+    w_buffer = aemet_weather()
 
     if w_buffer is not None:
         return make_response(jsonify({'buffer':w_buffer}),200)
     abort(404)
 
 @app.route('/price/<type>')
-def get_current_price(type):    
+def get_current_price(type):
     price = None
 
     if type == 'PVPC':
        price = pvpc_price()
     elif type == 'SPOT':
         price = spot_price()
-        
+
     if price is not None:
         return make_response(jsonify({'type':type, 'unit':'Kwh', 'price':price}),200)
     abort(404)
@@ -43,4 +43,4 @@ def not_found(error):
 
 
 if __name__=="__main__":
-    app.run(debug=True, host='0.0.0.0',port=7000)    
+    app.run(debug=True, host='0.0.0.0',port=7000)
