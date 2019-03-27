982f39e50582dbdd9dfc4ef7c45571c23ac2dd2a
pablopalomino96 <pablo.palomino1@alu.uclm.es>
2019-03-27 16:00:59 +0100

    pylint passed in api_esios.py

--- SOURCE/app/api_esios.py
+++ SOURCE/app/api_esios.py
@@ -1,36 +1,38 @@
 #!/usr/bin/python3
 # -*- coding: utf-8; mode: python -*-
-
-import datetime
-import requests
+''' e-sios API module
+Note: The functionality of this module is to provide a buffer with the next 24 PVPC or SPOT price values, as indicated
+'''
 import datetime as dt
 import random as rd
+import requests
 import project_constants as const
 
-headers = {
-          'Accept' : 'application/json; application/vnd.esios-api-v1+json',
-          'Content-Type' : 'application/json',
-          'Host' : 'api.esios.ree.es',
-          'Authorization' : 'Token token=\"' + const.ESIOS_TOKEN + '\"'
-          }
+HEADERS = {
+    'Accept' : 'application/json; application/vnd.esios-api-v1+json',
+    'Content-Type' : 'application/json',
+    'Host' : 'api.esios.ree.es',
+    'Authorization' : 'Token token=\"' + const.ESIOS_TOKEN + '\"'
+}
 
 
 def get_incoming_prices(indicator):
-    global headers
     start = const.START
     end = const.END
-    url = const.ESIOS_URL.replace('$INDICATOR',indicator).replace('$START_DATE',dt.datetime.strftime(start,'%Y/%m/%d')).replace('$END_DATE',dt.datetime.strftime(end,'%Y/%m/%d'))
-    response = requests.get(url, headers=headers)
+
+    url = const.ESIOS_URL.replace('$INDICATOR', indicator)
+    url = url.replace('$START_DATE', dt.datetime.strftime(start, '%Y/%m/%d'))
+    url = url.replace('$END_DATE', dt.datetime.strftime(end, '%Y/%m/%d'))
+
+    response = requests.get(url, headers=HEADERS)
     if response.status_code == 200:
         data = response.json()
-        price_buffer = create_price_buffer(data,start)
+        price_buffer = create_price_buffer(data, start)
         return price_buffer
-    else:
-        print("An error has occurred")
-        return None
+    return None
 
-def create_price_buffer(data,start):
-    pb = []
+def create_price_buffer(data, start):
+    price_buffer = []
     pb_size = 0
     price_per_hours = data['indicator']['values']
 
@@ -38,21 +40,13 @@ def create_price_buffer(data,start):
         price_date = dt.datetime.strptime(price['datetime'].split('.')[0], "%Y-%m-%dT%H:%M:%S")
 
         if price['geo_name'] == 'España' and (price_date.date() > start.date() or (price_date.date() == start.date() and price_date.hour >= start.hour)):
-            pb.append(round(price['value']/1000,3)) # price in €/kwh
+            price_buffer.append(round(price['value']/1000, 3)) # price in €/kwh
             pb_size += 1
 
         if pb_size >= 24: break
 
-    # at 20:20 in PVPC case and 17:00 in SPOT case the information of the day D+1 is published. If it is not available yet, then complete the remaining hours of the buffer with a previous random value
     while pb_size < 24:
-        pb.append(rd.choice(pb))
+        price_buffer.append(rd.choice(price_buffer))
         pb_size += 1
 
-    return pb
-
-'''
-if "__NAME__==__MAIN__":
-    print("Prices between {} and {}".format(const.START,const.END))
-    print(get_incoming_prices(const.PVPC))
-    print(get_incoming_prices(const.SPOT))
-'''
+    return price_buffer
