679b3576875a623b8a60aa739c55ad85feb1ec2f
Pablo Palomino Gomez <pablo.palomino1@outlook.com>
2019-05-13 15:28:56 +0000

    move database to DB2 in IBM Cloud

--- SOURCE/app/config/prod_config.py
+++ SOURCE/app/config/prod_config.py
@@ -1,9 +1,8 @@
 import os
 
-PWD = os.path.abspath(os.curdir)
 SECRET_KEY = os.urandom(10)
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'sqlite:///{}/db/dbase.db'.format(PWD)
+SQLALCHEMY_DATABASE_URI = 'db2://dwm91559:zswksj730cmk90%5Eq@dashdb-txn-sbox-yp-lon02-02.services.eu-gb.bluemix.net:50000/BLUDB'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 CONSUMPTIONS_FOLDER = 'consumptions'
 SIMULATIONS_FOLDER = 'simulations'
