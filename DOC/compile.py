9fba992e166cfb31afe047ec28e2726d2765cf9d
Pablo <Pablo.Palomino1@outlook.com>
2019-06-09 19:54:01 +0200

    Review changes and other doc

--- DOC/compile.py
+++ DOC/compile.py
@@ -6,6 +6,9 @@ commands = [
     ['pdflatex', sys.argv[1] + '.tex'],
     ['bibtex', sys.argv[1] + '.aux'],
     ['pdflatex', sys.argv[1] + '.tex'],
+    ['pdflatex', sys.argv[1] + '.tex'],
+    ['makeglossaries', sys.argv[1]],
+    ['pdflatex', sys.argv[1] + '.tex'],
     ['pdflatex', sys.argv[1] + '.tex']
 ]
 
